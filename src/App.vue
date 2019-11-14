1ac2964c753420e4814435b7a4da504208112f64
andreyromanov <andreygorogogo@gmail.com>
2019-11-14 22:15:11 +0200

    sidebar

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,30 @@
     <div id="nav">
       <div class="container">
         <div class="row">
-          <div class="col-md-4 border">
+          <div class="col-md-3 border py-2">
+            <img src="/author.jpg" class="rounded mx-auto d-block img-user" alt="Me">
+            <ul class="list-group mt-3">
+              <li class="list-group-item">
+                <i class="far fa-user"></i>
+                <span class="user-info">Andrii Romanov</span>
+              </li>
+              <li class="list-group-item">
+                <i class="fas fa-globe"></i>
+                <span class="user-info">Full-stack web developer</span>
+              </li>
+              <li class="list-group-item">
+                <i class="fab fa-linkedin"></i>
+                <span class="user-info">LinkedIn</span>
+              </li>
+              <li class="list-group-item">
+                <i class="far fa-map"></i>
+                <span class="user-info">Odesa, Ukraine</span>
+              </li>
+            </ul>
+          </div>
+          <div class="col-md-9 border">
             <router-link to="/">Home</router-link> |
             <router-link to="/about">About Me</router-link>
-          </div>
-          <div class="col-md-8">
             <router-view />
           </div>
         </div>
--- src/main.js
+++ src/main.js
@@ -9,6 +9,10 @@ Vue.use(BootstrapVue);
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 
+import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/js/all.js'
+
+
 Vue.config.productionTip = false;
 
 new Vue({

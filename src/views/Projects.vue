7277a1ecdb9ea84eb32e4066430d17fde3175422
andreyromanov <andreygorogogo@gmail.com>
2019-12-15 23:14:31 +0200

    Projects

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,12 +1,52 @@
 <template>
   <div class="projects">
   	<transition appear name="fade">
-    <ul class="mt-5">
-    	<li>UAtao</li>
-    	<li>Wrapcrowd</li>
-    	<li>Mebtex</li>
-    	<li>Skytex</li>
-    </ul>
+    <div class="row mt-5">
+    	<div class="col-md-6 col-xs-12 mb-3">
+    		<div class="card">
+                  <a class="card-a" href="#">
+                    <div id="ut" class="card-body cards">
+                        <h2 class="card-title">UA-TAO</h2>
+                    </div>
+                  </a>
+            </div>
+        </div>
+    	<div class="col-md-6 col-xs-12 mb-3">
+	    	<div class="card">
+	                  <a class="card-a" href="#">
+	                    <div id="wc" class="card-body cards">	                        
+					        <img
+				              src="/logo.png"
+				              class="rounded mx-auto d-block"
+				              alt="Me" style="width: 50%;"
+				            />
+	                    </div>
+	                  </a>
+	            </div>
+	    </div>
+    	<div class="col-md-6 col-xs-12 mb-3">
+	    	<div class="card">
+	                  <a class="card-a" href="#">
+	                    <div id="mt" class="card-body cards">
+	                        <img
+				              src="/mebtexlogo.png"
+				              class="rounded mx-auto d-block"
+				              alt="Me" style="width: 50%;"
+				            />
+	                    </div>
+	                  </a>
+	            </div>
+	    </div>
+	    <div class="col-md-6 col-xs-12 mb-3">
+	    	<div class="card">
+	                  <a class="card-a" href="#">
+	                    <div id="st" class="card-body cards">
+	                        <h2 class="card-title">Skytex</h2>
+	                    </div>
+	                  </a>
+	            </div>
+	    </div>
+    </div>
 </transition>
   </div>
 </template>

f83ef81486e75aa52ec6df7b3769fa80f5a25d32
andreyromanov <andreygorogogo@gmail.com>
2020-01-02 13:07:35 +0200

    Projects description

--- src/views/projects/skytex.vue
+++ src/views/projects/skytex.vue
@@ -1,9 +1,32 @@
 <template>
 	<div>
-		<img
-              src="/projects/skytex/st-1-min.png"
-              class="rounded mx-auto d-block proj-img"
-              alt="Me"
-            />
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6 py-3">
+					<h4>Used tools</h4>
+					<span class="badge badge-success mr-3">Wordpress</span>
+        			<span class="badge badge-success mr-3">Html</span>
+        			<span class="badge badge-success mr-3">Css</span>
+        			<span class="badge badge-primary mr-3">Php</span>
+        			<span class="badge badge-primary mr-3">Adobe Photoshop</span>
+				</div>
+				<div class="col-md-6 py-3">
+					<h4>Description</h4>
+					<p class="text-left">Andrii Romanov is a skilled web developer, who is specialized in structuring and developing websites. 
+	
+					Andrii enjoys doing both frontend and backend jobs. 
+	
+					In frontend skills, Andrii is known for an excellent application of HTML, CSS, and Bootstrap. 
+					</p>
+				</div>
+				<div class="col-md-12">
+				<img
+	              src="/projects/skytex/st-1-min.png"
+	              class="rounded mx-auto d-block proj-img"
+	              alt="Me"
+	            />
+				</div>
+			</div>
+		</div>
 	</div>
 </template>
--- src/views/projects/u-build.vue
+++ src/views/projects/u-build.vue
@@ -1,5 +1,35 @@
 <template>
 	<div>
-		ubuild
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6 py-3">
+					<h4>Used tools</h4>
+					<span class="badge badge-success mr-3">Php</span>
+					<span class="badge badge-success mr-3">Laravel</span>
+        			<span class="badge badge-success mr-3">Html</span>
+        			<span class="badge badge-success mr-3">Css</span>
+        			<span class="badge badge-primary mr-3">JavaScript</span>
+        			<span class="badge badge-primary mr-3">MySQL</span>
+        			<span class="badge badge-primary mr-3">Jquery</span>
+        			<span class="badge badge-primary mr-3">Bootstrap</span>
+				</div>
+				<div class="col-md-6 py-3">
+					<h4>Description</h4>
+					<p class="text-left">Andrii Romanov is a skilled web developer, who is specialized in structuring and developing websites. 
+	
+					Andrii enjoys doing both frontend and backend jobs. 
+	
+					In frontend skills, Andrii is known for an excellent application of HTML, CSS, and Bootstrap. 
+					</p>
+				</div>
+				<div class="col-md-12">
+				<img
+	              src="/projects/mebtex/mt-1-min.png"
+	              class="rounded mx-auto d-block proj-img"
+	              alt="Me"
+	            />
+				</div>
+			</div>
+		</div>
 	</div>		
 </template>	

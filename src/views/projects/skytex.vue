7fc6137a2183dad0ad79c56fa0e95a17c956c9b9
andreyromanov <andreygorogogo@gmail.com>
2020-01-29 21:27:53 +0200

    Skytex/Mebtex description

--- src/views/projects/skytex.vue
+++ src/views/projects/skytex.vue
@@ -13,7 +13,7 @@
 				<div class="col-md-6 py-3">
 					<h4>Description</h4>
 					<p class="text-left">
-					Skytex
+					Skytex is a curtain delivery company. A website was developed using CMS Wordpress. During the work, some files were modified using php, html and css. Developed home page and contact page. Also in the course of work, Adobe Photoshop was used to process photos of goods.
 					</p>
 				</div>
 				<div v-lazy-container="{ selector: 'img' }" class="col-md-6">
